ffcfa0a57dd664e8adfc46eb8c1c06d25baef35a
ChrisSyrnyk <60796272+ChrisSyrnyk@users.noreply.github.com>
2022-05-16 20:02:58 -0600

    Delete Webservice directory

--- Webservice/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Webservice.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
